d4f3328614bf9db94197e21e9a3ecf9257abd158
enpassos <49312777+enpassos@users.noreply.github.com>
2023-12-01 12:18:13 -0300

    Add files via upload

--- cmems_faoc.py
+++ cmems_faoc.py
@@ -1,16 +1,35 @@
 import os
 import sys
+import datetime as dt
 
 a = os.getcwd()
-print('Diretorio: ',a)
 
+print('Diretorio: ',a)
+os.mkdir('./Output')
+os.mkdir('./downloads')
+
+hoje = dt.datetime.now()
+os.chdir('./scripts')
+arq = 'download_cmems.sh'
+if hoje.day > 15:
+    with open(arq) as r:
+        text = r.read().replace('mes_inicio=`date -d "-2 month" +%Y-%m`','mes_inicio=`date -d "-1 month" +%Y-%m`').replace('mes_fim=`date -d "-1 month" +%Y-%m`','mes_fim=`date -d "" +%Y-%m`')
+    with open(arq,'w') as w:
+        w.write(text)
+elif hoje.day < 15:
+    with open(arq) as r:
+        text = r.read().replace('mes_inicio=`date -d "-1 month" +%Y-%m`','mes_inicio=`date -d "-2 month" +%Y-%m`').replace('mes_fim=`date -d "" +%Y-%m`','mes_fim=`date -d "-1 month" +%Y-%m`')
+    with open(arq,'w') as w:
+        w.write(text)
+
+os.chdir('../')
+os.chmod("./scripts/download_cmems.sh", 755)
 os.system("sh ./scripts/download_cmems.sh")
+
 print(' ')
 print('Download realizado com sucesso!')
 print(' ')
 print('Gerando figuras para o site: ')
 os.system("python ./scripts/faoc_figuras_site_biod.py")
 
-# Colocar aqui para chamar o script "faoc_figuras_site_biod.py"
-# Deletar teste.sh download_cmems.sh environment.yml 
-# Fazer a pasta de volume
+
